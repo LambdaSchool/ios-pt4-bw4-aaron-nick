465c6a2d79f910ef961818240ce4cbd20995b274
Aaron Haughton <dreamcast2000@outlook.com>
2020-06-16 23:59:53 -0400

    initial commit; Xcode project created.

---
+++ MeetUpAttendance/MeetUpAttendance/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MeetUpAttendance
+//
+//  Created by Joe on 6/16/20.
+//  Copyright © 2020 LambdaSchool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MeetUpAttendance/MeetUpAttendanceTests/MeetUpAttendanceTests.swift
@@ -0,0 +1,34 @@
+//
+//  MeetUpAttendanceTests.swift
+//  MeetUpAttendanceTests
+//
+//  Created by Joe on 6/16/20.
+//  Copyright © 2020 LambdaSchool. All rights reserved.
+//
+
+import XCTest
+@testable import MeetUpAttendance
+
+class MeetUpAttendanceTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

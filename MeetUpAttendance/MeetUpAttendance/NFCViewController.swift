3af5422c1a2ac7250112bb9f9aa864808b2674ec
Nicolas <nickrios33@yahoo.com>
2020-06-17 20:46:13 -0700

    added code for a basic nfc tag  and added nfc entitlements to enable scanner funtionality

--- MeetUpAttendance/MeetUpAttendance/NFCViewController.swift
+++ MeetUpAttendance/MeetUpAttendance/NFCViewController.swift
@@ -7,16 +7,58 @@
 //
 
 import UIKit
+import CoreNFC
 
-class NFCViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+class NFCViewController: UIViewController, NFCNDEFReaderSessionDelegate {
+    
+    @IBOutlet weak var textView: UITextView!
+    
+   
+   
+    var nfcSession: NFCNDEFReaderSession?
+    
+    
+    @IBAction func ScanPressed(_ sender: Any) {
+        nfcSession = NFCNDEFReaderSession.init(delegate: self, queue: nil, invalidateAfterFirstRead: true)
+        nfcSession?.begin()
+        
+        
     }
     
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+        print("The session was invlidated: \(error.localizedDescription)")
+    }
+    
+    func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+        var result = ""
+        for payload in messages[0].records{
+            print("-> \(payload)")
+            result += String.init(data: payload.payload.advanced(by: 3), encoding: .ascii) ?? "Format not supported"
+        }
+        DispatchQueue.main.async {
+            self.textView.text = result
+            
+        }
+        
+        
+        
+    }
+ 
+    
    
-
-}
+        
+        
+        
+        
+    }
